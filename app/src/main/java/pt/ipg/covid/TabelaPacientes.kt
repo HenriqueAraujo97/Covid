fd46a6e9bb8520ef607341c21e87c65baabd65bf
HenriqueAraujo97 <araujo.henrique311@gmail.com>
2021-05-19 19:49:22 +0100

    Criadas todas as funções CRUD da TabelaPacientes

--- app/src/main/java/pt/ipg/covid/TabelaPacientes.kt
+++ app/src/main/java/pt/ipg/covid/TabelaPacientes.kt
@@ -1,5 +1,7 @@
 package pt.ipg.covid
 
+import android.content.ContentValues
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.provider.BaseColumns
@@ -11,8 +13,28 @@ class TabelaPacientes(db: SQLiteDatabase) {
         db.execSQL("CREATE TABLE $NOME_TABELA (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $NOME_PACIENTE TEXT NOT NULL, $IDADE_PACIENTE INTEGER NOT NULL, $NR_PACIENTE INTEGER NOT NULL)")
     }
 
+    fun insert(values: ContentValues): Long{
+        return  db.insert(NOME_TABELA, null, values)
+    }
 
+    fun update(values: ContentValues, whereClause: String, whereArgs: Array<String>): Int {
+        return db.update(NOME_TABELA, values, whereClause, whereArgs)
+    }
 
+    fun delete(whereClause: String, whereArgs: Array<String>): Int {
+        return db.delete(NOME_TABELA, whereClause, whereArgs)
+    }
+
+    fun query(
+        columns: Array<String>,
+        selection: String?,
+        selectionArgs: Array<String>?,
+        groupBy: String?,
+        having: String?,
+        orderBy: String?
+    ): Cursor? {
+        return db.query(NOME_TABELA, columns, selection, selectionArgs, groupBy, having, orderBy)
+    }
 
 
     companion object{
@@ -20,6 +42,8 @@ class TabelaPacientes(db: SQLiteDatabase) {
         const val NOME_PACIENTE = "nome"
         const val IDADE_PACIENTE = "idade"
         const val NR_PACIENTE = "numero"
+
+        val TODAS_COLUNAS = arrayOf(BaseColumns._ID, NOME_PACIENTE, IDADE_PACIENTE, NR_PACIENTE)
     }
 }
 
