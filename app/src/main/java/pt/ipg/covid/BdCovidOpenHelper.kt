8eb47bba4a1e074e90f0ccaa1726c5e2391ba65a
HenriqueAraujo97 <araujo.henrique311@gmail.com>
2021-05-19 19:34:17 +0100

    Base Dados criada

---
+++ app/src/main/java/pt/ipg/covid/BdCovidOpenHelper.kt
@@ -0,0 +1,23 @@
+package pt.ipg.covid
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class BdCovidOpenHelper(context: Context?) : SQLiteOpenHelper(context, NOME_BASE_DADOS,null, VERSAO_BASE_DADOS) {
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        if(db != null){
+
+        }
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+
+    companion object{
+        const val NOME_BASE_DADOS = "covid.db"
+        const val VERSAO_BASE_DADOS = 1
+    }
+}
