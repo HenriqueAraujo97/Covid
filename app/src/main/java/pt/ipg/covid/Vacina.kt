30b3be12daf30e4527912d218a19687183fe9cb0
HenriqueAraujo97 <araujo.henrique311@gmail.com>
2021-06-09 01:20:23 +0100

    Companion  object criado da tabela Vacina

--- app/src/main/java/pt/ipg/covid/Vacina.kt
+++ app/src/main/java/pt/ipg/covid/Vacina.kt
@@ -1,6 +1,8 @@
 package pt.ipg.covid
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 import java.util.*
 
 data class Vacina(var id: Long = -1, var data:  Date, var local: String, var idVacina: Long) {
@@ -11,5 +13,21 @@ data class Vacina(var id: Long = -1, var data:  Date, var local: String, var idV
             put(TabelaVacinas.CAMPO_ID_PACIENTE,idVacina)
         }
         return valores
+    }
+
+    companion object{
+        fun fromCursor(cursor: Cursor) : Vacina{
+            val colId = cursor.getColumnIndex(BaseColumns._ID)
+            val colData = cursor.getColumnIndex(TabelaVacinas.CAMPO_DATA)
+            val colLocal = cursor.getColumnIndex(TabelaVacinas.CAMPO_LOCAL)
+            val colIdPaciente = cursor.getColumnIndex(TabelaVacinas.CAMPO_ID_PACIENTE)
+
+            val id = cursor.getLong(colId)
+            val data = cursor.getLong(colData)
+            val local = cursor.getString(colLocal)
+            val idVacina = cursor.getLong(colIdPaciente)
+
+            return Vacina( id, data, local, idVacina)
+        }
     }
 }
