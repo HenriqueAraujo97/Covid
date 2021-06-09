e141c0deec4bcd679a16c3ad8f96123ac79dd9b2
HenriqueAraujo97 <araujo.henrique311@gmail.com>
2021-06-09 01:33:43 +0100

    Feita a coneção a base dados e criado o companion object do ContentProviderPaciente

--- app/src/main/java/pt/ipg/covid/ContentProviderPaciente.kt
+++ app/src/main/java/pt/ipg/covid/ContentProviderPaciente.kt
@@ -1,4 +1,58 @@
 package pt.ipg.covid
 
-class ContentProviderPaciente {
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+import kotlin.reflect.KTypeProjection
+
+class ContentProviderPaciente : ContentProvider() {
+    private var bdCovidOpenHelper : BdCovidOpenHelper? = null
+    override fun onCreate(): Boolean {
+        bdCovidOpenHelper = BdCovidOpenHelper(context)
+        return true
+    }
+
+    override fun query(
+        uri: Uri,
+        projection: Array<out String>?,
+        selection: String?,
+        seletionArgs: Array<out String>?,
+        sortOrder: String?
+    ): Cursor? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getType(p0: Uri): String? {
+        TODO("Not yet implemented")
+    }
+
+    override fun insert(p0: Uri, p1: ContentValues?): Uri? {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(p0: Uri, p1: String?, p2: Array<out String>?): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun update(p0: Uri, p1: ContentValues?, p2: String?, p3: Array<out String>?): Int {
+        TODO("Not yet implemented")
+    }
+
+    companion object{
+        private const val AUTHORITY= "pt.ipg.covid"
+
+        private const val PACIENTES = "pacientes"
+        private const val ENFERMEIROS = "enfermeiros"
+        private const val VACINAS = "vacinas"
+
+        private  const val MULTIPLOS_ITEMS = "vnd.android.cursor.dir"
+        private  const val UNICO_ITEMS = "vnd.android.cursor.item"
+
+        private val ENDEREÇO_BASE = Uri.parse("content://$AUTHORITY")
+        private val ENDEREÇO_PACIENTES = Uri.withAppendedPath(ENDEREÇO_BASE, PACIENTES)
+        private val ENDEREÇO_ENFERMEIROS = Uri.withAppendedPath(ENDEREÇO_BASE, ENFERMEIROS)
+        private val ENDEREÇO_VACINAS = Uri.withAppendedPath(ENDEREÇO_BASE, VACINAS)
+    }
+
 }
