6b344f7553b94e8ea0a0bbbd309828f2685ee8e6
HenriqueAraujo97 <araujo.henrique311@gmail.com>
2021-06-29 22:03:58 +0100

    Criao o layout vacina com as classes para mostrar a atividade

--- app/src/main/java/pt/ipg/covid/MainActivity.kt
+++ app/src/main/java/pt/ipg/covid/MainActivity.kt
@@ -1,11 +1,38 @@
 package pt.ipg.covid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun verPaciente(view: View){
+
+        val intentPaciente = Intent(this,MostrarPaciente::class.java)
+
+        startActivity(intentPaciente)
+
+    }
+
+    fun verEnfermeiro(view: View){
+
+        val intentEnfermeiro = Intent(this,MostrarEnfermeiro::class.java)
+
+        startActivity(intentEnfermeiro)
+
+    }
+
+    fun verVacina(view: View){
+
+        val intentVacina = Intent(this,MostrarVacina::class.java)
+
+        startActivity(intentVacina)
+
     }
 }
---
+++ app/src/main/java/pt/ipg/covid/MostrarEnfermeiro.kt
@@ -0,0 +1,16 @@
+package pt.ipg.covid
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MostrarEnfermeiro : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.enfermeiros)
+
+    }
+
+
+}
